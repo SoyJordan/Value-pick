ca1e58059dfc3cc72091a4129a546c59b9badc91
SoyJordan <ksj79bk8ys@privaterelay.appleid.com>
2026-08-26 23:40:21 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="soyjordan-v1806";
+const CACHE="soyjordan-v1807";
 const STATIC=["./manifest.json","./sj-shield-v1798.jpg","./sj-shield-v1798.svg","./sj-shield-180-v1798.png","./sj-shield-192-v1798.png","./sj-shield-512-v1798.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
